bc6a87bbc4dcedaa025056862ea704a6cde762c0
preetish02 <sainipreetish02@gmail.com>
2025-02-08 21:53:20 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -7,7 +7,7 @@ int main(){
     else if(ch == 'B'){printf("Good");}
     else if(ch == 'C'){printf("Average");}
     else if(ch == 'D'){printf("Below Average");}
-    else if(ch == 'F'){printd("Fail");}
+    else if(ch == 'F'){printf("Fail");}
     else{printf("Invalid grade");}
     return 0;
 }
