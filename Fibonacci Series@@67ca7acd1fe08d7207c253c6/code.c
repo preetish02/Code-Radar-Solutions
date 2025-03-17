9d7ba138face5393638e6db93769a2eb64184327
Preetish Saini <sainipreetish02@gmail.com>
2025-03-17 16:59:15 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,14 @@
+#include<Stdio.h>
+int fibo(n){
+    if(n == 0)
+    return 0;
+    else if(n==1)
+    return 1;
+    else 
+    return fibo(n-1)+ fibo(n-2);
+}
+int main(){
+    int a;
+    printf("%d",fibo(a));
+    return 0;
+}
